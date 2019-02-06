a84d70813da7ada3fb1a9ec359989b89bb5c1895
sowmiyashakthi <37236217+sowmiyashakthi@users.noreply.github.com>
2019-02-06 15:08:13 +0530

    Create armstrong.py

---
+++ beginner/armstrong.py
@@ -0,0 +1,10 @@
+sum =0 
+temp = num
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10
+if num==sum:
+    print(num,"is armstrong")
+else:
+    print(num,"is not armstrong")
